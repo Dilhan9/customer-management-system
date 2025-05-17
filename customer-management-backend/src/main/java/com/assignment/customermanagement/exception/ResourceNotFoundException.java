d4bde83ac9c46ccc3cf491dfea14671c8cab31cc
Dilhan9 <45040107+Dilhan9@users.noreply.github.com>
2025-05-17 19:56:37 +0530

    Delete customer-management-backend directory

--- customer-management-backend/src/main/java/com/assignment/customermanagement/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.assignment.customermanagement.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
